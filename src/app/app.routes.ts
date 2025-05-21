1d1d6d9fac666051dc42988efc91afb2b43142cf
Yudish Juwaheer <60716609+yjuwaheer@users.noreply.github.com>
2025-05-20 22:42:13 -0230

    Added base components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { OrderComponent } from './order/order.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'order', component: OrderComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: 'order' },
+];
---
+++ src/app/order/order.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order',
+  imports: [],
+  templateUrl: './order.component.html',
+  styleUrl: './order.component.scss',
+})
+export class OrderComponent {}
