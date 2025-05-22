15e775c01a86631d33375587c9c9fa13766e4450
Yudish Juwaheer <60716609+yjuwaheer@users.noreply.github.com>
2025-05-21 23:28:40 -0230

    Added base layout for order options

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import data from '../shared/data/data.json';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-order',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './order.component.html',
   styleUrl: './order.component.scss',
 })
-export class OrderComponent {}
+export class OrderComponent implements OnInit {
+  desserts = data;
+
+  ngOnInit(): void {
+    console.log(this.desserts);
+  }
+}
