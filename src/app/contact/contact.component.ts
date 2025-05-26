ddc33845f7b5d58e374bbb5b6d713bd73389326b
Yudish Juwaheer <60716609+yjuwaheer@users.noreply.github.com>
2025-05-25 23:33:51 -0230

    Completed contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  private formBuilder = inject(FormBuilder);
+
+  contactForm = this.formBuilder.group({
+    firstName: ['', [Validators.required, Validators.minLength(1)]],
+    lastName: ['', [Validators.required, Validators.minLength(1)]],
+    email: ['', [Validators.required, Validators.minLength(1)]],
+    inquiryType: ['', [Validators.required, Validators.minLength(1)]],
+    message: ['', [Validators.required, Validators.minLength(1)]],
+    consent: [false, [Validators.required, Validators.requiredTrue]],
+  });
+
+  showSuccessMessage = false;
+  showErrorMessage = false;
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      this.showSuccessMessage = true;
+      this.showErrorMessage = false;
+      this.contactForm.reset();
+      setTimeout(() => {
+        this.showSuccessMessage = false;
+      }, 3000);
+    } else {
+      this.showSuccessMessage = false;
+      this.showErrorMessage = true;
+      setTimeout(() => {
+        this.showErrorMessage = false;
+      }, 3000);
+    }
+  }
+}
