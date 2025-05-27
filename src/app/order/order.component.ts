286baa6b2d6abc7dac123d1a827ba20849db8981
Yudish Juwaheer <60716609+yjuwaheer@users.noreply.github.com>
2025-05-26 23:04:22 -0230

    Added logic to add an item to cart

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -10,8 +10,30 @@ import { CurrencyPipe } from '@angular/common';
 })
 export class OrderComponent implements OnInit {
   desserts = data;
+  cart: {
+    itemId: number;
+    quantity: number;
+  }[] = [];
 
-  ngOnInit(): void {
-    console.log(this.desserts);
+  ngOnInit() {}
+
+  getItemInCart(selectedItemId: number) {
+    return this.cart.find(item => item.itemId === selectedItemId);
+  }
+
+  addToCart(selectedItemId: number) {
+    const itemInCart = this.cart.find(item => item.itemId === selectedItemId);
+    if (!itemInCart) {
+      this.cart.push({
+        itemId: selectedItemId,
+        quantity: 1
+      })
+    } else {
+      itemInCart.quantity += 1;
+    }
+  }
+
+  removeFromCart(selectedItemId: number) {
+    console.log(selectedItemId);
   }
 }
